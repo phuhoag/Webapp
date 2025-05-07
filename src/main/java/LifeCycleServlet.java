57e52268b9de1de6a1f1d589493b4a6c04afb67a
phuhoag <11a8hoangkimquyphu@gmail.com>
2025-05-07 17:14:22 +0700

    them thong bao cho moi phuong thuc va kiem tra console va them servlet LifeCycleServlet

--- src/main/java/LifeCycleServlet.java
+++ src/main/java/LifeCycleServlet.java
@@ -1,4 +1,4 @@
-package com.example;
+
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
@@ -6,30 +6,30 @@ import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
 
-@WebServlet("/greeting")
-public class HelloServlet extends HttpServlet {
+@WebServlet("/lifecycle")
+public class LifeCycleServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        System.out.println("HelloServlet: init() called");
+        System.out.println("LifeCycleServlet: init() called");
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        System.out.println("HelloServlet: doGet() called");
+        System.out.println("LifeCycleServlet: doGet() called");
         response.setContentType("text/html");
-        response.getWriter().println("<h1>Hello Phu</h1>");
+        response.getWriter().println("<h1>This is LifeCycleServlet</h1>");
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        System.out.println("HelloServlet: doPost() called");
+        System.out.println("LifeCycleServlet: doPost() called");
         response.setContentType("text/html");
-        response.getWriter().println("<h1>Post request received</h1>");
+        response.getWriter().println("<h1>LifeCycleServlet POST request</h1>");
     }
 
     @Override
     public void destroy() {
-        System.out.println("HelloServlet: destroy() called");
+        System.out.println("LifeCycleServlet: destroy() called");
     }
 }
